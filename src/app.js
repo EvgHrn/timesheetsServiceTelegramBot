fbaa88e4bc89d3df19ea26bd1f77ae2437daa3a8
EvgHrn <evg.hrn@gmail.com>
2022-02-15 09:56:16 +0400

    cors

--- src/app.js
+++ src/app.js
@@ -32,6 +32,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
